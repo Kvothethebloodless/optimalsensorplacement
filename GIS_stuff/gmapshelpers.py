0d01dce3b39b615653e76c49ec176afdc14469db
kvothethebloodless <manish97ravula@gmail.com>
2016-06-27 23:33:36 +0530

    Tile Retreival

--- GIS_stuff/gmapshelpers.py
+++ GIS_stuff/gmapshelpers.py
@@ -1,18 +1,13 @@
 from __future__ import division
-from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
 import pdb
 
-TILE_SIZE = 256;
-MAX_ZOOM = 23;
-def latlngToPixel((lat,lon),zoom):
-    #Returns the pixel coordinate of a certain point at the required zoom level.
-    (worldX,worldY) = latlngToWorld((lat,lon))
-    (pixelX,pixelY) = worldToPixel((worldX,worldY),zoom)
-    return (pixelX,pixelY)
+TILE_SIZE = 256
+MAX_ZOOM = 23
 
-def latlngToWorld((lat,lon)):
+
+def latlngToWorld((lat, lon)):
 #     pdb.set_trace()
     #Copied from https://developers.google.com/maps/documentation/javascript/examples/map-coordinates
     siny = np.sin(lat*np.pi/180)
@@ -22,11 +17,19 @@ def latlngToWorld((lat,lon)):
     return mercpoint*TILE_SIZE
     #Works!
 
-def latlngToTile((lat,lng),zoom):
+def latlngToPixel((lat,lon),z):
+
+    #Returns the pixel coordinate of a certain point at the required zoom level.
+    (worldX,worldY) = latlngToWorld((lat,lon))
+    (pixelX,pixelY) = worldToPixel((worldX,worldY),z)
+    return (pixelX,pixelY)
+
+
+def latlngToTile((lat,lng),z):
     #Returns cordinates of the tile involving the latitude and longitude at that zoom level
     pdb.set_trace()
     (worldX,worldY) = latlngToWorld((lat,lng))
-    (pixelX,pixelY) = worldToPixel((worldX,worldY),zoom)
+    (pixelX,pixelY) = worldToPixel((worldX,worldY),z)
     (tileX,tileY) = pixelToTile((pixelX,pixelY))
     return (tileX,tileY)
 
@@ -39,36 +42,82 @@ def pixelToTile((pixelX,pixelY)):
     else:
         raise ValueError('Invalid zoom value. Must be under 21.')
         return
-def tileToPixel((tileX,tileY)):
-   #Gives top left corner pixel value. 
-    pixelX = tileX*TILE_SIZE
-    pixelY = tileY*TILE_SIZE
-    return (pixelX,pixelY)
-#def tileToCenterPixel((tileX,tileY)):
-    #Returns center pixel of tile
-#    (pixelX,pixelY) = tileToPixel(tileX,tileY);
-#    return (pixelX+128,pixelY+128)
 
 
-
-def worldToPixel((worldX,worldY),zoom):
-    validrequest = False
-    _validrequest = True if zoom<=MAX_ZOOM else False
+def worldToPixel((worldX,worldY),z):
+    _validrequest = False
+    _validrequest = True if z<=MAX_ZOOM else False
     if _validrequest:
-        xcord = np.floor(worldX*np.power(2,zoom))
-        ycord = np.floor(worldY*np.power(2,zoom))
+        xcord = np.floor(worldX*np.power(2,z))
+        ycord = np.floor(worldY*np.power(2,z))
         return np.asarray((xcord,ycord),dtype=int)
     else:
         raise ValueError('Invalid zoom value. Must be under 21.')
         return
 
-    
+def tileToPixel((tileX,tileY)):
+   #Gives top left corner pixel value. 
+    pixelX = tileX*TILE_SIZE
+    pixelY = tileY*TILE_SIZE
+    return (pixelX,pixelY)
+
+
+def tileToCenterPixel((tileX,tileY)):
+   # Returns center pixel of tile
+    (pixelX,pixelY) = tileToPixel((tileX,tileY));
+    return (pixelX+128,pixelY+128)
+
 
 def pixelToWorld((pixel_x,pixel_y),z):
+    #Converts pixel coordinates at a zoom level to world coordinates
+    if np.any(np.abs(np.array((pixel_x,pixel_y)))>np.power(2,z+8)):
+        raise ValueError('Invalid Pixel value for given zoom')
+        return
+
     scale = np.float(np.power(2,z))
     world_x = np.float(pixel_x) / scale
     world_y = np.float(pixel_y) / scale
     return (world_x,world_y)
+
+
+def pixelToLatlng((pixelX,pixelY),z):
+    (worldX,worldY) = pixelToWorld((pixelX,pixelY),z)
+    return worldToLatLon((worldX,worldY))
+
+def tileToLatlng((tileX,tileY),z):
+    if np.any(np.array((tileX,tileY)>np.power(2,z))):
+        raise ValueError('Invalid tile value for the given zoom')
+        return
+    centrepixel = tileToCenterPixel((tileX,tileY))
+    latlng = pixelToLatlng_better(centrepixel,z)
+    print ('Latitude and Longitude of the centre pixel %s are %s')%(centrepixel,latlng)
+    return latlng
+
+
+
+def pixelToLatlng_better((pixelX,pixelY),z):
+    #For better calulcation. By default, the pixeltoLatLng converts to the Lat Long 
+    # corresponding to the top righ corner of the pixel. So for better result, we are
+    #infact calculating all the four sides (Different pixels corners, infact) and
+    # averaging them up.
+    w = pixelToWorld((pixelX,pixelY),z)
+    list_sides = []
+    list_sides.append(w)
+    list_sides.append(pixelToWorld((pixelX, pixelY+1),z))
+    list_sides.append(pixelToWorld((pixelX+1, pixelY),z))
+    list_sides.append(pixelToWorld((pixelX+1, pixelY+1),z))
+    ele1 = 0
+    ele2 = 0
+    for i in range(4):
+        print(list_sides[i])
+        ele1 += list_sides[i][0]
+        ele2 += list_sides[i][1]
+    w_new  = (ele1/4,ele2/4)
+    print w_new
+    latlng_final = worldToLatLon(w_new)
+    print(latlng_final)
+    return (latlng_final) 
+
 
 
 def worldToLatLon((world_x,world_y)):
